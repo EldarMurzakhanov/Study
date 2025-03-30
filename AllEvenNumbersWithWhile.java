92b6cb2b6b28bf98c7584ebe825b7b9c644f892f
e.murzakhanov <eldarmurzahanov@gmail.com>
2025-03-31 01:13:40 +0300

    Cum mit

---
+++ AllEvenNumbersWithWhile.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class AllEvenNumbersWithWhile {
+
+    public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        int value = getValidInput(scanner);
+        System.out.println("Ваше число " + value);
+        int i = 0;
+
+        if (value >= 0) {
+            while(i <= value) {
+                System.out.println("Чётное число " + i);
+                i = i + 2;
+            }
+        } else {
+            while(i >= value) {
+                System.out.println("Чётное число " + i);
+                i = i - 2;
+            }
+        }
+    }
+
+    private static int getValidInput(Scanner scanner) {
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.out.println("Попробуйте в этот раз ввести число");
+        }
+        return scanner.nextInt();
+    }
+}
+
