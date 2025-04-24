c8136c395454af95b52c651cd50c17c4bc0d568f
e.murzakhanov <eldarmurzahanov@gmail.com>
2025-04-25 01:30:44 +0300

    Rewrite step 8

--- LearningCurve/StringsAndArraysPalindrome.java
+++ LearningCurve/StringsAndArraysPalindrome.java
@@ -11,12 +11,13 @@ public class StringsAndArraysPalindrome {
         // 2) Просим пользователя ввести фразу
         System.out.println("Введите фразу.");
 
-        // 3) Заворачиваем введённое значение в стрингу и сохраняем.
+        // 3) Заворачиваем введённое значение в стрингу и сохраняем. Приводим стрингу к виду нижнего регистра.
+
         String phrase = userInput.nextLine();
-        String phraseLowerCase = phrase.toLowerCase();
+        String phraseRefined = phrase.toLowerCase().replaceAll("\\s", "");
 
         // 4) Выводим сохранённую фразу.
-        System.out.println("Вы ввели: " + phraseLowerCase);
+        System.out.println("Вы ввели: " + phrase);
 
         // 5) Инициализируем переменные, в которые будем сохранять согласные и гласные буквы.
         int consonantsAmount = 0;
@@ -28,24 +29,24 @@ public class StringsAndArraysPalindrome {
                 'б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'
         };
 
-        // 8) Объявляем переменную, в которой будем массив беззвучных букв.
+        // 8) Объявляем переменную, в которой будем массив гласных.
         char[] vowels = {
                 'а', 'е', 'ё', 'и', 'о', 'у', 'э', 'ю', 'я'
         };
 
         // 8) Запускаем циклы, которые будут ходить по массиву, вытаскивать согласные и гласные буквы и сохранять их в
         // переменные.
-        for (int i = 0; i < phraseLowerCase.length(); i++) {
+        for (int i = 0; i < phraseRefined.length(); i++) {
             for (int j = 0; j < consonants.length; j++) {
-                if (phraseLowerCase.charAt(i) == consonants[j]) {
+                if (phraseRefined.charAt(i) == consonants[j]) {
                     consonantsAmount ++;
                 }
             }
         }
 
-        for (int i = 0; i < phraseLowerCase.length(); i++) {
+        for (int i = 0; i < phraseRefined.length(); i++) {
             for (int j = 0; j < vowels.length; j ++) {
-                if (phraseLowerCase.charAt(i) == vowels[j]) {
+                if (phraseRefined.charAt(i) == vowels[j]) {
                     vowelsAmount ++;
                 }
             }
@@ -57,12 +58,20 @@ public class StringsAndArraysPalindrome {
 
         // 10) Написать логику проверки палиндрома. Уже на опыте, будем использовать цикл, который ходит по половине
         // строки и проверяет, является ли символ в начале строки копией символа в конце строки.
+        boolean isPalindrome = true;
 
+        char[] phraseCharArray = phraseRefined.toCharArray();
 
-        for (int i = 0; i < phraseLowerCase.length()/2; i++) {
-            if (phraseLowerCase[i] == phraseLowerCase.length() - 1 - i) {
-
+        for (int i = 0; i < phraseRefined.length()/2; i++) {
+            if (phraseCharArray[i] != phraseCharArray[phraseRefined.length() - 1 - i]) {
+                isPalindrome = false;
+                System.out.println("Ваша строка не палиндром");
+                break;
             }
+        }
+
+        if (isPalindrome == true) {
+            System.out.println("Ваша строка палиндром");
         }
     }
 }
