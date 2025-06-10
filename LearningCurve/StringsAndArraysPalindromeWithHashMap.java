f1d9a9136436d7337ec6eeee10eb3f9d3b264c64
e.murzakhanov <eldarmurzahanov@gmail.com>
2025-06-10 03:20:34 +0300

    Улучшенная реализация с хэшмапой

--- LearningCurve/StringsAndArraysPalindromeWithHashMap.java
+++ LearningCurve/StringsAndArraysPalindromeWithHashMap.java
@@ -12,11 +12,8 @@ public class StringsAndArraysPalindromeWithHashMap {
         // 2) Просим пользователя ввести фразу
         System.out.println("Введите фразу.");
 
-        // 3) Заворачиваем введённое значение в стрингу и сохраняем. Приводим стрингу к виду нижнего регистра. Приводим
-        // стрингу к массиву чаров.
-
+        // 3) Заворачиваем введённое значение в стрингу и сохраняем.
         String phrase = userInput.nextLine();
-        String phraseRefined = phrase.toLowerCase().replaceAll("\\s", "");
 
         // 4) Выводим сохранённую фразу.
         System.out.println("Вы ввели: " + phrase);
@@ -25,51 +22,50 @@ public class StringsAndArraysPalindromeWithHashMap {
         int consonantsAmount = 0;
         int vowelsAmount = 0;
 
-        // 7) Объявляем hashmap.
+        // 6) Объявляем hashmap.
+        HashMap<Character, Byte> lettersMap = new HashMap<>();
 
-        HashMap<Character, String> lettersMap = new HashMap<>();
+        // 7) Заполняем hashmap дынными
+        lettersMap.put('а', (byte) 1);
+        lettersMap.put('б', (byte)  0);
+        lettersMap.put('в', (byte)  0);
+        lettersMap.put('г', (byte)  0);
+        lettersMap.put('д', (byte)  0);
+        lettersMap.put('е', (byte)  1);
+        lettersMap.put('ё', (byte)  1);
+        lettersMap.put('ж', (byte)  0);
+        lettersMap.put('з', (byte)  0);
+        lettersMap.put('и', (byte)  1);
+        lettersMap.put('й', (byte)  0);
+        lettersMap.put('к', (byte)  0);
+        lettersMap.put('л', (byte)  0);
+        lettersMap.put('м', (byte)  0);
+        lettersMap.put('н', (byte)  0);
+        lettersMap.put('о', (byte)  1);
+        lettersMap.put('п', (byte)  0);
+        lettersMap.put('р', (byte)  0);
+        lettersMap.put('с', (byte)  0);
+        lettersMap.put('т', (byte)  0);
+        lettersMap.put('у', (byte)  1);
+        lettersMap.put('ф', (byte)  0);
+        lettersMap.put('х', (byte)  0);
+        lettersMap.put('ц', (byte)  0);
+        lettersMap.put('ч', (byte)  0);
+        lettersMap.put('ш', (byte)  0);
+        lettersMap.put('щ', (byte)  0);
+        lettersMap.put('э', (byte)  1);
+        lettersMap.put('ю', (byte)  1);
+        lettersMap.put('я', (byte)  1);
 
-        // 8) Заполняем hashmap дынными
-
-        lettersMap.put('а', "vowels");
-        lettersMap.put('б', "consonants");
-        lettersMap.put('в', "consonants");
-        lettersMap.put('г', "consonants");
-        lettersMap.put('д', "consonants");
-        lettersMap.put('е', "vowels");
-        lettersMap.put('ё', "vowels");
-        lettersMap.put('ж', "consonants");
-        lettersMap.put('з', "consonants");
-        lettersMap.put('и', "vowels");
-        lettersMap.put('й', "consonants");
-        lettersMap.put('к', "consonants");
-        lettersMap.put('л', "consonants");
-        lettersMap.put('м', "consonants");
-        lettersMap.put('н', "consonants");
-        lettersMap.put('о', "vowels");
-        lettersMap.put('п', "consonants");
-        lettersMap.put('р', "consonants");
-        lettersMap.put('с', "consonants");
-        lettersMap.put('т', "consonants");
-        lettersMap.put('у', "vowels");
-        lettersMap.put('ф', "consonants");
-        lettersMap.put('х', "consonants");
-        lettersMap.put('ц', "consonants");
-        lettersMap.put('ч', "consonants");
-        lettersMap.put('ш', "consonants");
-        lettersMap.put('щ', "consonants");
-        lettersMap.put('э', "vowels");
-        lettersMap.put('ю', "vowels");
-        lettersMap.put('я', "vowels");
-
-        // 9) Запускаем цикл, которые будут ходить по массиву, и извелкать нужные значения, ориентируясь на ключи в мапе.
-        for (int i = 0; i < phraseRefined.length(); i++) {
-            if (lettersMap.containsKey(phraseRefined.charAt(i))) {
-                if (lettersMap.get(phraseRefined.charAt(i)).equals("vowels")) {
-                    vowelsAmount ++;
-                }
-                if (lettersMap.get(phraseRefined.charAt(i)).equals("consonants")) {
-                    consonantsAmount ++;
+        // 8) Запускаем цикл, которые будут ходить по массиву, и извелкать нужные значения, ориентируясь на ключи в мапе.
+        for (int i = 0; i < phrase.length(); i++) {
+            char letter = phrase.charAt(i);
+            if (lettersMap.containsKey(letter)) {
+                byte value = lettersMap.get(letter);
+                if (value == 1) {
+                    vowelsAmount++;
+                } else if (value == 0) {
+                    consonantsAmount++;
                 }
             }
         }
@@ -78,16 +74,18 @@ public class StringsAndArraysPalindromeWithHashMap {
         System.out.println("Количество согласных: " + consonantsAmount);
         System.out.println("Количество гласных: " + vowelsAmount);
 
-        // 10) Написать логику проверки палиндрома. Уже на опыте, будем использовать цикл, который ходит по половине
+        // 10) Написать логику проверки палиндрома. Приводим стрингу к виду нижнего регистра. Приводим
+        // стрингу к массиву чаров. Уже на опыте, будем использовать цикл, который ходит по половине
         // строки и проверяет, является ли символ в начале строки копией символа в конце строки.
+        String phraseRefined = phrase.toLowerCase().replaceAll("\\s", "");
+        int len = phraseRefined.length();
+
         boolean isPalindrome = true;
 
-        char[] phraseCharArray = phraseRefined.toCharArray();
-
-        for (int i = 0; i < phraseRefined.length()/2; i++) {
-            if (phraseCharArray[i] != phraseCharArray[phraseRefined.length() - 1 - i]) {
-                isPalindrome = false;
+        for (int i = 0; i < len / 2; i++) {
+            if (phraseRefined.charAt(i) != phraseRefined.charAt(len - 1 - i)) {
                 System.out.println("Ваша строка не палиндром");
+                isPalindrome = false;
                 break;
             }
         }
