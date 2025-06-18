6d87d776ad74c2ce6a51906e85b2b5fcebfdf9c8
e.murzakhanov <eldarmurzahanov@gmail.com>
2025-06-19 01:35:49 +0300

    Рефакторинг словаря

--- LearningCurve/StringsAndArraysPalindromeWithHashMap.java
+++ LearningCurve/StringsAndArraysPalindromeWithHashMap.java
@@ -13,7 +13,7 @@ public class StringsAndArraysPalindromeWithHashMap {
         System.out.println("Введите фразу.");
 
         // 3) Заворачиваем введённое значение в стрингу и сохраняем.
-        String phrase = userInput.nextLine();
+        String phrase = userInput.nextLine().toLowerCase();
 
         // 4) Выводим сохранённую фразу.
         System.out.println("Вы ввели: " + phrase);
@@ -23,48 +23,25 @@ public class StringsAndArraysPalindromeWithHashMap {
         int vowelsAmount = 0;
 
         // 6) Объявляем hashmap.
-        HashMap<Character, Byte> lettersMap = new HashMap<>();
+        HashMap<Character, Boolean> lettersMap = new HashMap<>();
 
-        // 7) Заполняем hashmap дынными
-        lettersMap.put('а', (byte) 1);
-        lettersMap.put('б', (byte)  0);
-        lettersMap.put('в', (byte)  0);
-        lettersMap.put('г', (byte)  0);
-        lettersMap.put('д', (byte)  0);
-        lettersMap.put('е', (byte)  1);
-        lettersMap.put('ё', (byte)  1);
-        lettersMap.put('ж', (byte)  0);
-        lettersMap.put('з', (byte)  0);
-        lettersMap.put('и', (byte)  1);
-        lettersMap.put('й', (byte)  0);
-        lettersMap.put('к', (byte)  0);
-        lettersMap.put('л', (byte)  0);
-        lettersMap.put('м', (byte)  0);
-        lettersMap.put('н', (byte)  0);
-        lettersMap.put('о', (byte)  1);
-        lettersMap.put('п', (byte)  0);
-        lettersMap.put('р', (byte)  0);
-        lettersMap.put('с', (byte)  0);
-        lettersMap.put('т', (byte)  0);
-        lettersMap.put('у', (byte)  1);
-        lettersMap.put('ф', (byte)  0);
-        lettersMap.put('х', (byte)  0);
-        lettersMap.put('ц', (byte)  0);
-        lettersMap.put('ч', (byte)  0);
-        lettersMap.put('ш', (byte)  0);
-        lettersMap.put('щ', (byte)  0);
-        lettersMap.put('э', (byte)  1);
-        lettersMap.put('ю', (byte)  1);
-        lettersMap.put('я', (byte)  1);
+        // 7) Строка гласных
+        String vowels = "аеёиоуыэюя";
+
+        // 8) Исключаем 'ъ' и 'ь' при добавлении
+        for (char c = 'а'; c <= 'я'; c++) {
+            if (c != 'ъ' && c != 'ь') {
+                lettersMap.put(c, vowels.indexOf(c) != -1);
+            }
+        }
 
         // 8) Запускаем цикл, которые будут ходить по массиву, и извелкать нужные значения, ориентируясь на ключи в мапе.
         for (int i = 0; i < phrase.length(); i++) {
             char letter = phrase.charAt(i);
             if (lettersMap.containsKey(letter)) {
-                byte value = lettersMap.get(letter);
-                if (value == 1) {
+                if (lettersMap.get(letter)) {
                     vowelsAmount++;
-                } else if (value == 0) {
+                } else {
                     consonantsAmount++;
                 }
             }
@@ -77,7 +54,7 @@ public class StringsAndArraysPalindromeWithHashMap {
         // 10) Написать логику проверки палиндрома. Приводим стрингу к виду нижнего регистра. Приводим
         // стрингу к массиву чаров. Уже на опыте, будем использовать цикл, который ходит по половине
         // строки и проверяет, является ли символ в начале строки копией символа в конце строки.
-        String phraseRefined = phrase.toLowerCase().replaceAll("\\s", "");
+        String phraseRefined = phrase.replaceAll("[^а-яё]", "");
         int len = phraseRefined.length();
 
         boolean isPalindrome = true;
